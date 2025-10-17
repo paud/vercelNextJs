3cba0acd706b23c64bd4afb7d2796a3ea6b80d7c
paud <5974721+paud@users.noreply.github.com>
2025-10-17 20:12:10 +0900

    1

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,6 +5,6 @@ const prisma = new PrismaClient();//.$extends(withAccelerate());
 
 const globalForPrisma = global as unknown as { prisma: typeof prisma };
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+console.log("Using Accelerate:", !!process.env.PRISMA_ACCELERATE_URL);
 
 export default prisma;
