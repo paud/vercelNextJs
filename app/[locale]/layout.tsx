89b93958d2201923d922e3076c217d758a02b2bd
paud <5974721+paud@users.noreply.github.com>
2025-10-24 19:41:48 +0900

    修正不能部署vercel的问题

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,9 +4,8 @@ import Header from '../Header';
 import Footer from '../Footer';
 import Providers from '../providers';
 
-export default async function LocaleLayout({ children, params }: { children: React.ReactNode; params: { locale: string } }) {
-  const { locale } = params;
-  console.log('locale:', locale); // 调试输出
+export default async function LocaleLayout({ children, params }: { children: React.ReactNode; params: Promise<{ locale: string }> }) {
+  const { locale } = await params;
   const messages = await getMessages({ locale });
   return (
     <Providers>
